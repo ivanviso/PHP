10c455d438d57c89084ba5b8497e04e1c4c93bc8
Ivan Viso <ivisoaltamirano@danielcastelao.org>
2020-10-27 18:08:48 +0100

    a

--- Arrays/1.php
+++ Arrays/1.php
@@ -1,9 +1,19 @@
 <?php
+echo "\n A \n";
 for ($i=0; $i < 3; $i++) { 
+    echo "\n";
     for ($j=0; $j < 5; $j++) { 
     $tresPorCinco[$i][$j]=rand();
     echo "fila ", $i+1, " columna ", $j+1," : ", $tresPorCinco[$i][$j], "\n";
     }
 }
 
+echo "\n B \n";
+for ($j=0; $j < 5; $j++) { 
+    echo "\n";
+    for ($i=0; $i < 3; $i++) { 
+    $tresPorCinco[$i][$j]=rand();
+    echo "columna ", $j+1, " fila ", $i+1," : ", $tresPorCinco[$i][$j], "\n";
+    }
+}
 ?>
