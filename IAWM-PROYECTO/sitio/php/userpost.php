c118ed4f6f7373aa111012dfc6e8fbbada072191
ivanviso123@gmail.com <ivanviso123@gmail.com>
2021-01-26 17:07:21 +0100

    sda

---
+++ IAWM-PROYECTO/sitio/php/userpost.php
@@ -0,0 +1,10 @@
+<html>
+<?php
+    $username=$_REQUEST['username'];
+    $pass=$_REQUEST['pass']
+    $nombre=$_REQUEST['nombre']
+    $addr=$_REQUEST['addr']
+    $dni=$_REQUEST['dni']
+    echo $username,$pass,$addr,$nombre,$dni;
+?>
+</html>
