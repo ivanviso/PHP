1b41ed3a3d407c4f9d10921bc97970466370574c
ivanviso <ivanviso123@gmail.com>
2021-02-11 17:08:25 +0100

    l

--- IAWM-PROYECTO/sitio/php/insert.php
+++ IAWM-PROYECTO/sitio/php/insert.php
@@ -1,13 +1,15 @@
 <?php
 $query_prueba="SELECT count(usuario) from usuario as cuenta where usuario='$username'";  // comprobamos que no exiten otros usuarios con el mismo nombre.
-$query="INSERT INTO usuario(usuario,contrasinal,direccion,telefono,nifdni,fecha_registro) VALUES ('$username','$hashpass','$addr',$tlf,'$dni',NOW())"; // necesitamos insertar en dos tablas.
-$query2="INSERT INTO novo_rexistro(usuario,contrasinal,direccion,telefono,nifdni) VALUES ('$username','$hashpass','$addr',$tlf,'$dni')";
+$query="INSERT INTO usuario(usuario,contrasinal,direccion,telefono,nifdni,fecha_registro,nome) VALUES ('$username','$hashpass','$addr',$tlf,'$dni',NOW(),'$nombre')"; // necesitamos insertar en dos tablas.
+$query2="INSERT INTO novo_rexistro(usuario,contrasinal,direccion,telefono,nifdni,nome) VALUES ('$username','$hashpass','$addr',$tlf,'$dni','$nombre')";
 
 include 'php/tests.php'; // realizamos las comprobaciones de las variables en este fichero. 
 
 if (empty($errores)) { //insertamos en la base de datos, si no hay ningun error. 
     mysqli_query($conn,$query);
     mysqli_query($conn,$query2); 
+    include 'php/redirect.php';
+    js_redirect('/login.php'); // nos lleva a la pagina de login si todo va bien. 
 }
 mysqli_commit($conn);
 mysqli_close($conn);
--- IAWM-PROYECTO/sitio/registro.php
+++ IAWM-PROYECTO/sitio/registro.php
@@ -1,7 +1,7 @@
 <html>
 
 <head>
-    <link rel="stylesheet" href="css/login.csss">
+    <link rel="stylesheet" href="css/login.css">
     <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
     <meta http-equiv="Pragma" content="no-cache" />
     <meta http-equiv="Expires" content="0" />
